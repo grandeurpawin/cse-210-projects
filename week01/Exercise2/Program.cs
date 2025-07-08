38583635e961e68268518a161cf362ccae416117
grandeurpawin <gpawin@byupathwat.edu>
2025-07-08 13:25:38 +0800

    Programming Excercises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,85 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        string sign = "";
+        int last_digit = percentage % 10;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (percentage < 60)
+        {
+            letter = "F";
+        }
+
+        else
+        {
+            Console.WriteLine("Invalid grade percentage entered. ");
+        }
+
+
+        if (last_digit >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (last_digit < 3)
+        {
+            sign = "-";
+        }
+
+        else 
+        {
+            sign = "";
+        }
+
+        if (percentage >= 93)
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+
+        else
+        {
+            Console.WriteLine("Keep going, there is  brighter future ahead. ");
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,76 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.Write("Enter a list of numbers, type 0 when finished. \n");
+
+        List<int> seriesNumbers = new List<int>();
+
+        int number = 1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter  number: ");
+            number = int.Parse(Console.ReadLine());
+
+
+
+            if (number != 0)
+            {
+                seriesNumbers.Add(number);
+                var result_set = seriesNumbers.OrderBy(num => num);
+            }
+
+            foreach (int num in seriesNumbers)
+            {
+
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int num in seriesNumbers)
+        {
+            sum += num;
+        }
+        Console.Write($"The sum is: {sum}");
+
+        float average = (float)sum / seriesNumbers.Count;
+        Console.Write($"\nThe average is: {average}");
+
+        int max = seriesNumbers[0];
+
+        foreach (int num in seriesNumbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        int min = int.MaxValue;
+
+        foreach (int num in seriesNumbers)
+        {
+            if (num > 0 && num < min)
+            {
+                min = num;
+            }
+        }
+
+        Console.Write($"\nThe largest number is: {max}");
+        Console.Write($"\nThe smallest positive number is: {min}");
+
+
+        Console.WriteLine("\nThe sorted list is: ");
+        foreach (int num in seriesNumbers.OrderBy(n => n))
+        {
+            Console.WriteLine($"{num} ");
+        }
     }
 }
