ebeb50da7c770e92047c79a80fdf703934c93e06
grandeurpawin <gpawin@byupathwat.edu>
2025-08-14 18:28:49 +0800

    Polymorphism Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        {
+            Square s1 = new Square("Red", 5);
+            shapes.Add(s1);
+
+            Circle s2 = new Circle("Blue", 3);
+            shapes.Add(s2);
+
+            Rectangle s3 = new Rectangle("Green", 4, 6);
+            shapes.Add(s3);
+        };
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shapes has an area of {area}.");
+        }
     }
 }
